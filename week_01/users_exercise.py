449840e2a9fcd2c281bb81d343082400fbdc6af7
Paul <paul.dennehy@hotmail.com>
2023-07-07 12:35:01 +0100

    now completed task 5 still do 6 after skipping to 7

--- week_01/users_exercise.py
+++ week_01/users_exercise.py
@@ -63,6 +63,10 @@ print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
 print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
+eriks_lottery_num_list = users["Erik"]["lottery_numbers"]
+eriks_lottery_num_list_sorted = sorted(eriks_lottery_num_list)
+
+print(eriks_lottery_num_list_sorted[0])
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"]["lottery_numbers"].append(7)
