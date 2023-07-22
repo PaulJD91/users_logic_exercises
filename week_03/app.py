761af81987f91e0e9b5bf71cd91c56b5e22e3bfa
Paul <paul.dennehy@hotmail.com>
2023-07-22 20:36:03 +0100

    started book class

---
+++ week_03/app.py
@@ -0,0 +1,7 @@
+from book import Book
+
+book1 = Book("Stealheart", "Brandon Sanderson", "Scifi")
+book2 = Book("Exile", "Raymond Feist", "Fantasy")
+book3 = Book("Animal Farm", "George Orwell", "Political Satire")
+
+book_list = [book1, book2, book3]
