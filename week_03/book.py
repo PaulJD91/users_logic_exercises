2fe06b539b299acabdb92edbd55b7554b6c7ba6f
Paul <paul.dennehy@hotmail.com>
2023-07-24 08:46:02 +0100

    added function names, not sure on how to create full function

--- week_03/book.py
+++ week_03/book.py
@@ -4,7 +4,19 @@ class Book:
         self.title = title
         self.author = author
         self.genre = genre
-        
+    
+
+    def add_to_list(self):
+        pass
+
+    def remove_from_list(self):
+        pass
+
+    def get_individual_book(self):
+        pass
+
+    def print_list_books(self):
+        pass
 
     
 
